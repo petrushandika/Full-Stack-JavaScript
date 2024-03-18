4b0bf35dc784698035f13611a16fd10785a039fe
petrushandika <petrushandikasinaga@gmail.com>
2024-03-18 16:59:36 +0700

    Practice Quiz ChatGPT

---
+++ 00-Practice Quiz/01 Primitive/04 Comparison Operator/quiz.js
@@ -0,0 +1,39 @@
+// 1. What is the result of `(Math.sqrt(25) * 4) > (20 + 5)`?
+let resultOne = (Math.sqrt(25) * 4 ) > (20 + 5);
+console.log(resultOne);
+
+// 2. Determine whether the following statement is true or false: `("apple".toUpperCase() === "APPLE") && (10 !== 20)`.
+let resultTwo = ("apple".toUpperCase() === "APPLE") && (10 !== 20);
+console.log(resultTwo);
+
+// 3. Evaluate the expression: `(8 ** 2 <= 100) || (15 >= 20 + 5)`?
+let resultThree = (8 ** 2 <= 100) || (15 >= 20 + 5);
+console.log(resultThree);
+
+// 4. Does the expression `(20 % 3) === (15 / 3)` evaluate to true or false?
+let resultFour = (20 % 3) === (15 / 3);
+console.log(resultFour);
+
+// 5. Evaluate the expression: `(5 ** 2 + 1) !== (10 * 3)`?
+let resultFive = (5 ** 2 + 1) !== (10 * 3);
+console.log(resultFive);
+
+// 6. Determine if the following statement is true or false: `("cat".length < "dog".length) && (15 === 15)`?
+let resultSix = ('cat'.length < 'dog'.length) && (15 === 15);
+console.log(resultSix);
+
+// 7. Calculate whether `(100 / 2) === (50 * 2) - 25`.
+let resultSeven = (100 / 2) === (50 * 2) - 25;
+console.log(resultSeven);
+
+// 8. Determine the truth value of `(true && false) || (true && !false)`.
+let resultEight = (true && false) || (true && !false);
+console.log(resultEight);
+
+// 9. Evaluate if `(10 + 5) * 2 >= (3 ** 2 + 5)` is true or false.
+let resultNine = (10 + 5) * 2 >= (3 ** 2 + 5);
+console.log(resultNine);
+
+// 10. Does `(20 % 4) !== (15 / 2 + 1)` yield true or false?
+let resultTen = (20 % 4) !== (15 / 2 + 1);
+console.log(resultTen);
