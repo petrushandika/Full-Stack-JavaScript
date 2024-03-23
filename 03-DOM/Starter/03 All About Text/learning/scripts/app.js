155e48571038380135952520a7ea2a15e9df09a3
petrushandika <petrushandikasinaga@gmail.com>
2024-03-23 19:41:58 +0700

    Document Object Model (DOM)

---
+++ 03-DOM/Starter/03 All About Text/learning/scripts/app.js
@@ -0,0 +1,11 @@
+// InnerText
+const h1 = document.querySelector('h1');
+h1.innerText = 'Text Changed';
+
+// textContent
+const p = document.querySelector('p');
+console.log(p.textContent);
+
+// innerHTML
+const another = document.querySelector('.another');
+another.innerHTML = '<em>This is inner HTML</em>'
