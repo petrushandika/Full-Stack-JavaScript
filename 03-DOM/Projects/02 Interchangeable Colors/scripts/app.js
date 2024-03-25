4ca274df564161eadfc0ac50eb676a2ac2bbb5e1
petrushandika <petrushandikasinaga@gmail.com>
2024-03-25 08:02:15 +0700

    DOM Project

---
+++ 03-DOM/Projects/02 Interchangeable Colors/scripts/app.js
@@ -0,0 +1,36 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.body;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        color = button.value;
+        changeBackground(color);
+    });
+});
+
+function changeBackground(color) {
+    body.className = '';
+
+    switch(color) {
+        case 'purple':
+            body.classList.add('purple');
+            break;
+        case 'blue':
+            body.classList.add('blue');
+            break;
+        case 'red':
+            body.classList.add('red');
+            break;
+        case 'green':
+            body.classList.add('green');
+            break;
+        case 'yellow':
+            body.classList.add('yellow');
+            break;
+        case 'teal':
+            body.classList.add('teal');
+            break;
+        default:
+            break;
+    }
+}
