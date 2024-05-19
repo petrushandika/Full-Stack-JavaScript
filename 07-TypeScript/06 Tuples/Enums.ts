e0cfeadbbdeafa832f0ba7dd73cc6e46607fd6b6
petrushandika <petrushandikasinaga@gmail.com>
2024-05-19 08:33:43 +0700

    Basics TypeScripts

---
+++ 07-TypeScript/06 Tuples/Enums.ts
@@ -0,0 +1,9 @@
+enum WeatherConditions {
+  Sunny = "sunny",
+  Cloudy = "cloudy",
+  Rainy = "rainy",
+  Snowy = "snowy",
+}
+
+const currentWeather = WeatherConditions.Snowy;
+console.log(`The current weather is ${currentWeather}`);
---
+++ 07-TypeScript/06 Tuples/Tuples.ts
@@ -0,0 +1,12 @@
+let myTuple: [string, number] = ["hello", 22];
+let [first, second] = myTuple
+console.log(first);
+console.log(second);
+
+const products: (number | string)[] = ['items', 21];
+
+console.log(products);
+
+const games: [string, number, boolean] = ['Game 1', 360, true];
+
+console.log(games);
