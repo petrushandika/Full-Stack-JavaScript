d62e187d86e16b3165b60ccf7551e80a5e8cd9f5
petrushandika <petrushandikasinaga@gmail.com>
2024-08-21 13:35:18 +0700

    Vue CLI

---
+++ 11-VueJs/projects/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app">
+    <h1>{{ title }}</h1>
+    <div>
+      <input
+        type="text"
+        ref="name"
+      />
+      <button @click="handleClick">Click Me</button>
+    </div>
+    <AppModal />
+  </div>
+</template>
+
+<script>
+import AppModal from "./components/AppModal.vue";
+
+export default {
+  name: "app",
+  components: { AppModal },
+  data() {
+    return {
+      title: "My First Vue App",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<style>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ 11-VueJs/projects/src/components/AppModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background-color: #fff;
+  border-radius: 10px;
+}
+</style>
