79061ff3f08d68f30da229e68ad537eef93989d6
petrushandika <petrushandikasinaga@gmail.com>
2024-08-24 13:49:14 +0700

    Form and Input

---
+++ 11-VueJs/03-Project/form-input/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <SignUpForm />
+</template>
+
+<script>
+import SignUpForm from "./components/SignUpForm.vue";
+
+export default {
+  name: "app",
+  components: { SignUpForm },
+};
+</script>
+
+<style></style>
