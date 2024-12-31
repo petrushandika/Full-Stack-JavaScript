ab760db52615eb9d71da4375ec4f9e1045d120ed
petrushandika <petrushandikasinaga@gmail.com>
2024-12-31 14:51:50 +0700

    Reactive Values

--- 12-Svelte/01 Basics/src/App.svelte
+++ 12-Svelte/01 Basics/src/App.svelte
@@ -1,6 +1,14 @@
 <script>
-	let name = 'Petrus';
+	let firstName = 'Petrus';
+	let lastName = 'Handika';
 	let colour = 'Black';
+
+	$: fullName = `${firstName} ${lastName}`
+	$: {
+		console.log(colour);
+		console.log(fullName);
+	}
+
 	const handleClick = () => {
 		colour = 'Orange';
 	};
@@ -11,11 +19,10 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p style="color: {colour}">{colour} Belt</p>
-	<button on:click={handleClick}>Update</button>
-	<!-- <input type="text" on:input={handleInput} value={colour}> -->
-	 <input type="text" bind:value={colour}>
+	<p>{fullName} - {colour} Belt</p>
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}>
+	<input type="text" bind:value={colour}>
 </main>
 
 <style>
@@ -24,13 +31,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
